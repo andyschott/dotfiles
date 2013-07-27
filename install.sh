e05dfe46f63d521bf8d3f090ea1a5d4b06a64adb
Andy Schott <schottac@gmail.com>
2013-07-26 22:01:35 -0400

    remove BBEdit

--- install.sh
+++ install.sh
@@ -9,10 +9,3 @@ cp -f ./emacs ~/.emacs
 cp -f ./gitconfig ~/.gitconfig
 
 cp -f ./gitignore ~/.gitignore
-
-dir=${PWD}
-rm -f ~/Library/Application\ Support\BBEdit
-
-pushd ~/Library/Application\ Support
-ln -s "$dir/BBEdit" BBEdit
-popd
